cb78b12c5b96d0aa001862729e9bb16d3d91963b
Felix Pape <Felix@Pape.rocks>
2020-08-31 20:08:06 +0200

    Connected with the PersistentDataSet and created a "findByProvince" functionality.

--- backend/export/src/main/java/com/f73/corona/backend/export/persistence/DatasetRepository.java
+++ backend/export/src/main/java/com/f73/corona/backend/export/persistence/DatasetRepository.java
@@ -1,4 +1,10 @@
 package com.f73.corona.backend.export.persistence;
 
-public class DatasetRepository {
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DatasetRepository extends CrudRepository<PersistentDataSet, String> {
+
+    PersistentDataSet findByProvince(String province);
+
 }
