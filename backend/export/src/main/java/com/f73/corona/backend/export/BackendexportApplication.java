b06befceb75133beb211a9c357eb538d2601f4f7
Felix Pape <Felix@Pape.rocks>
2020-08-31 14:20:23 +0200

    Created spring project for backend.export

---
+++ backend/export/src/main/java/com/f73/corona/backend/export/BackendexportApplication.java
@@ -0,0 +1,13 @@
+package com.f73.corona.backend.export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendexportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendexportApplication.class, args);
+    }
+
+}
---
+++ backend/export/src/test/java/com/f73/corona/backend/export/BackendexportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f73.corona.backend.export;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendexportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
