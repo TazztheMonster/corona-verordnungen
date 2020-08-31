5e8fe2633e4d7301a9ffa0c71ab48e455f182922
Felix Pape <Felix@Pape.rocks>
2020-08-31 18:33:08 +0200

    Removed unused imports

--- backend/export/src/main/java/com/f73/corona/backend/export/controller/webController.java
+++ backend/export/src/main/java/com/f73/corona/backend/export/controller/webController.java
@@ -2,14 +2,12 @@ package com.f73.corona.backend.export.controller;
 
 import com.f73.corona.backend.export.models.Dataset;
 import com.f73.corona.backend.export.models.Province;
-import com.f73.corona.backend.export.models.ProvinceData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
