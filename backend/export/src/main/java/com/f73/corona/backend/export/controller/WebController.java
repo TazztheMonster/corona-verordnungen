4bb5746d1aa40c305552b4c1f8cd448c65762d4b
Felix Pape <Felix@Pape.rocks>
2020-08-31 20:06:16 +0200

    Created the requestMapping for all BuildingTypes

--- backend/export/src/main/java/com/f73/corona/backend/export/controller/WebController.java
+++ backend/export/src/main/java/com/f73/corona/backend/export/controller/WebController.java
@@ -1,5 +1,6 @@
 package com.f73.corona.backend.export.controller;
 
+import com.f73.corona.backend.export.models.BuildingType;
 import com.f73.corona.backend.export.models.Dataset;
 import com.f73.corona.backend.export.models.Province;
 import org.springframework.http.HttpStatus;
@@ -14,23 +15,28 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RestController
-public class webController {
+public class WebController {
 
     @RequestMapping("/province/{province}")
     public Object getProvinceData(@PathVariable String province) {
 
-        Optional<Dataset> dataset = ProvinceData.getProvinceDataByString(province);
+        Optional<Dataset> dataset = DataController.getDatasetByProvince(province);
 
         if (dataset.isPresent()) {
-            return dataset.get();
+            return dataset;
         } else {
         return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
     }
 
-    @RequestMapping("/province")
+    @RequestMapping("/provinces")
     public List<String> getProvinces() {
         return Stream.of(Province.values()).map(Province::name).collect(Collectors.toList());
+    }
+
+    @RequestMapping("/buildingTypes")
+    public List<BuildingType> getAllBuildingTypes() {
+        return DataController.getAllBuildingTypes();
     }
 
 
