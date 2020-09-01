6b893e950779aa04be45ce49525f68725c154b47
Sido, Marlon (SE-A/51) <marlon.sido@volkswagen.de>
2020-09-01 10:27:12 +0200

    finished first iteration of import frontend and started working on Mainwindow of export frotend

--- frontend/import/src/app/mainwindow/mainwindow.component.ts
+++ frontend/import/src/app/mainwindow/mainwindow.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Province } from '../models/province'
 import { ProvinceData } from '../models/provinceData'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-mainwindow',
@@ -9,17 +10,39 @@ import { ProvinceData } from '../models/provinceData'
 })
 export class MainwindowComponent implements OnInit {
 
-  selectedProvince: String = '';
+  selectedProvince: Province = null;
 
   provinces: Province[] = [
     {name: 'Niedersachsen', personsIndoor: 10, personsOutdoor:50, householdsIndoor: 5, householdsOutdoor: 10, maskMandatory: true, buildingTypesClosed:["Schulen"], personsPsmIndoor: 1, personsPsmOutdoor: 2, other:''},
     {name: 'Nrw', personsIndoor: 8, personsOutdoor:40, householdsIndoor: 4, householdsOutdoor: 8, maskMandatory: false, buildingTypesClosed:["Schulen"], personsPsmIndoor: 2, personsPsmOutdoor: 5, other:''},
     {name: 'Bayern', personsIndoor: 12, personsOutdoor:50, householdsIndoor: 5, householdsOutdoor: 10, maskMandatory: true, buildingTypesClosed:["Biergärten"], personsPsmIndoor: 4, personsPsmOutdoor: 6, other:'Kein Bier macht uns traurig'}
-];;
+  ];
 
-  constructor() { }
+  buildingTypes: String[] = ["Restaurants", "Schulen", "Sporthallen", "Büros", "Biergärten"];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+  updateProvince(): void{
+    if(this.selectedProvince != null){
+      this.http.post('http://httpbin.org/post ', this.selectedProvince).toPromise().then(data => {
+        console.log(data);
+      });
+    }
+  }
+  updateAllProvinces(): void{
+  this.provinces.forEach(element => {
+    this.http.post('http://httpbin.org/post ', element).toPromise().then(data => {
+      console.log(data);
+    });
+  });
 
+  this.buildingTypes.forEach(element => {
+    this.http.post('http://httpbin.org/post ', element).toPromise().then(data => {
+      console.log(data);
+    });
+  })
 }
+}
+
