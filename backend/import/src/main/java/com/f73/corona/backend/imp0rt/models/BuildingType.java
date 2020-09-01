6093ee355aff24efc75277b9df8e11fc2adf4bf4
Felix Pape <Felix@Pape.rocks>
2020-09-01 11:27:31 +0200

    Copied from exportBackend

---
+++ backend/import/src/main/java/com/f73/corona/backend/imp0rt/models/BuildingType.java
@@ -0,0 +1,8 @@
+package com.f73.corona.backend.imp0rt.models;
+
+import lombok.Data;
+
+@Data
+public class BuildingType {
+    private String name;
+}
---
+++ backend/import/src/main/java/com/f73/corona/backend/imp0rt/models/Dataset.java
@@ -0,0 +1,22 @@
+package com.f73.corona.backend.imp0rt.models;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Dataset {
+
+    private BuildingType province;
+    private int personsIndoor;
+    private int personsOutdoor;
+    private int householdsIndoor;
+    private int householdsOutdoor;
+    private boolean maskMandatory;
+    private double personsPsmOutdoor;
+    private double personsPsmIndoor;
+    private List<BuildingType> closedBuildingTypes;
+
+
+
+}
