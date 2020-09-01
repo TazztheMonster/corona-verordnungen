41e7e30f29647345b9bb8c8c796627424520ab8a
Felix Pape <Felix@Pape.rocks>
2020-09-01 10:27:15 +0200

    Changed return Value to Optional

--- backend/export/src/main/java/com/f73/corona/backend/export/persistence/DatasetRepository.java
+++ backend/export/src/main/java/com/f73/corona/backend/export/persistence/DatasetRepository.java
@@ -3,8 +3,10 @@ package com.f73.corona.backend.export.persistence;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface DatasetRepository extends CrudRepository<PersistentDataSet, String> {
 
-    PersistentDataSet findByProvince(String province);
+    Optional<PersistentDataSet> findByProvince(String province);
 
 }
