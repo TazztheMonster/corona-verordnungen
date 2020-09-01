ee1b51f0752a590e1a3290df1644245a62cc7935
Felix Pape <Felix@Pape.rocks>
2020-09-01 15:25:03 +0200

    addet "findFirstByBuidingType"

--- backend/import/src/main/java/com/f73/corona/backend/imp0rt/persistence/BuildingTypeRepository.java
+++ backend/import/src/main/java/com/f73/corona/backend/imp0rt/persistence/BuildingTypeRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BuildingTypeRepository extends CrudRepository<PersistentBuildingType, Integer> {
 
     @Query("SELECT '*' FROM PersistentBuildingType")
     public List<PersistentBuildingType> getAll();
+
+    public Optional<PersistentBuildingType> findFirstByBuildingType(String buildingType);
 
 }
